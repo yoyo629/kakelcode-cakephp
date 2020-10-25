6e0185e0df3cb2b10e1f9073d1debbcb4f845ec3
yoyo629 <king.crimson.723.yu@gmail.com>
2020-10-25 17:33:54 +0900

    Fix: カウントダウンタイマーの処理方法を修正

--- webroot/js/cdt.js
+++ webroot/js/cdt.js
@@ -1,34 +1,49 @@
-function countDownTimer() {
+// 終了時間を取得->Auction/viewから入札終了時間を「end」に渡している
+var endDate = new Date(end);
+var interval = 1000;
 
-    var timer = setInterval(function () {
+function countdownTimer() {
+    //現在日時の取得
+    var nowDate = new Date();
+    //終了日時と現在日時の差異を算出
+    var period = endDate - nowDate;
+    //0を付けて右側2桁を返す
+    var addZero = function (n) { return ('0' + n).slice(-2); }
+    //3桁を表示する可能性のある日にちに適用する。
+    var addZeroDay = function (n) { return ('0' + n).slice(-3); }
 
-        //現在日時を取得
-        var nowDate = Date.now();
-        //入札終了時間を設定 view.ctpから「end」に入札終了時間を取得している
-        var anyDate = new Date(end);
-        //日数を計算
-        var daysBetween = Math.floor((anyDate - nowDate) / (24 * 60 * 60 * 1000));
-        var timeDifference = (anyDate - nowDate);
-        if (timeDifference >= 0) {
-            //時間を取得
-            var h = Math.floor(timeDifference / 3600000);
-            var _h = h % 24;
-            //分を取得
-            var m = Math.floor((timeDifference - h * 3600000) / 60000);
-            //秒を取得
-            var s = Math.round((timeDifference - h * 3600000 - m * 60000) / 1000);
+    //算出した差異を時刻として表示する処理
+    if (period >= 0) {
+        //日にちを取得
+        var day = Math.floor(period / (1000 * 60 * 60 * 24));
+        period -= (day * (1000 * 60 * 60 * 24));
+        //時間を取得
+        var hour = Math.floor(period / (1000 * 60 * 60));
+        period -= (hour * (1000 * 60 * 60));
+        //分を取得
+        var minutes = Math.floor(period / (1000 * 60));
+        period -= (minutes * (1000 * 60));
+        //秒を取得
+        var second = Math.floor(period / 1000);
 
-            //HTML上に出力
-            document.getElementById("countDown").innerHTML = daysBetween + "日と" + _h + "時間" + m + "分" + s + "秒";
-
-            if ((h == 0) && (m == 0) && (s == 0)) {
-                clearInterval(timer);
-                document.getElementById("countDown").innerHTML = "入札終了しました！";
-                alert('入札時間が終了しました！') ? '' : location.reload(); // 入札終了時間とともにアラートを表示してページをリロード
-            }
-        } else {
-            document.getElementById("countDown").innerHTML = "入札終了しました！"; //カウントダウンが終了していても表示するテキスト
+        //入札終了をアラートで宣言してページをリロードし、DBを更新する 
+        if (hour == 0 && minutes == 0 && second == 0) {
+            document.getElementById("countDown").innerHTML = "入札終了しました！";
+            alert('入札時間が終了しました！') ? '' : location.reload(); // 入札終了時間とともにアラートを表示してページをリロード
         }
-    }, 1000);
+        //HTMLで表示するためにcountに表示する時間を入れる
+        var count = "";
+        count += '<span class="h">' + addZeroDay(day) + '日' + '</span>';
+        count += '<span class="h">' + addZero(hour) + '時' + '</span>';
+        count += '<span class="m">' + addZero(minutes) + '分' + '</span>';
+        count += '<span class="s">' + addZero(second) + '秒' + '</span>';
+        document.getElementById('countDown').innerHTML = count;
+        setTimeout(countdownTimer, 10);
+    }
+    //カウントダウンが終了していても表示するテキスト
+    else {
+        document.getElementById("countDown").innerHTML = "入札終了しました！";
+    }
 }
-countDownTimer();
+
+countdownTimer();
